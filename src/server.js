fdc475e547817d2a9e5f7e59a48018986a75b761
guchur <74587816+gozldwnd1eo@users.noreply.github.com>
2022-05-17 14:13:27 +0900

    Room Count 1 commit

--- src/server.js
+++ src/server.js
@@ -16,6 +16,21 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app); //프로토콜이 다른 것을 한 서버에서 사용함
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const {
+      sockets: {
+        adapter: { sids, rooms },
+      },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+      if (sids.get(key) === undefined) {
+        publicRooms.push(key);
+      }
+    });
+    return publicRooms;
+  }
+
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anon";
     socket.onAny((event) => {
